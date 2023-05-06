0e4bfcde1634c2bd41e1126a1d1499edb051c74d
ismailbentabett <ismailbentabett@gmail.com>
2023-05-06 11:34:16 +0100

    added entities

---
+++ Domain/Image.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Path { get; set; }
+        public bool IsUploaded { get; set; }
+        public int PropertyId { get; set; }
+        public Property? Property { get; set; }
+    }
+}
